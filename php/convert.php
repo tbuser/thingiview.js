8a4f74fa17b4f4e29bf99039d0deec9336638500
Tony Buser <tbuser@gmail.com>
2011-01-04 01:37:46 -0500

    finished server side php conversion code, I still need to finish the examples and documentation

--- php/convert.php
+++ php/convert.php
@@ -31,22 +31,103 @@ function parse_stl_string($filename) {
           $vertexes[] = $vertex;
         }
         
-        // if (!$face_vertexes[$normal_count]) {
-        //   $face_vertexes[$normal_count] = array();
-        // }
-        
         $face_vertexes[$normal_count][] = $vertex;
       }
     }
   }
 
-  // build faces
+  $faces = make_faces($vertexes, $face_vertexes);
+  
+  // TODO: normals aren't really needed... should probably skip parsing them too
+  return array($vertexes, array(), $faces);
+}
+
+function parse_stl_binary($filename) {
+  $vertexes = array();
+  $normals  = array();
+  $faces    = array();
+
+  $face_vertexes = array();
+
+  $fp = fopen($filename, "rb");
+
+  // skip header
+  $data = fread($fp, 80);
+  $header = unpack("c*", $data);
+
+  // get number of faces
+  $data = fread($fp, 4);
+  $count = unpack("i", $data);
+
+  for ($i = 0; $i < $count[1]; $i++) {
+    // skip normals
+    $data = fread($fp, 12);
+    $normal = unpack("fff", $data);
+
+    for ($v_count = 0; $v_count < 3; $v_count++) {
+      $points = array();
+      
+      for ($v_index = 0; $v_index < 3; $v_index ++) {
+        $data = fread($fp, 4);
+        $points[] = unpack("f", $data);        
+      }
+
+      $vertex = array($points[0][1], $points[1][1], $points[2][1]);
+
+      if (!in_array($vertex, $vertexes)) {
+        $vertexes[] = $vertex;
+      }
+      
+      $face_vertexes[$i][] = $vertex;
+
+    }
+
+    $data = fread($fp, 2);
+    $attribute = unpack("S", $data);
+  }
+  
+  $faces = make_faces($vertexes, $face_vertexes);
+  
+  return array($vertexes, array(), $faces);
+}
+
+function parse_obj_string($filename) {
+  $lines    = file($filename);
+
+  $vertexes = array();
+  $normals  = array();
+  $faces    = array();
+
+  foreach($lines as $line) {
+    $line = preg_replace('/\s+/', ' ', $line);
+    
+    $parts = explode(' ', $line);
+    
+    switch ($parts[0]) {
+      case 'v':
+        $vertexes[] = array((float)$parts[1], (float)$parts[2], (float)$parts[3]);
+        break;
+      case 'f':
+        $face_point1_parts = explode('/', $parts[1]);
+        $face_point2_parts = explode('/', $parts[2]);
+        $face_point3_parts = explode('/', $parts[3]);
+
+        $faces[] = array((float)$face_point1_parts[0]-1, (float)$face_point2_parts[0]-1, (float)$face_point3_parts[0]-1);
+        break;
+    }
+  }
+  
+  return array($vertexes, array(), $faces);
+}
+
+function make_faces($vertexes, $face_vertexes) {
+  $faces = array();
+  
   foreach($face_vertexes as $index => $face_vertex) {
     $faces[$index] = array(array_search($face_vertex[0], $vertexes), array_search($face_vertex[1], $vertexes), array_search($face_vertex[2], $vertexes));
   }
   
-  // TODO: normals aren't really needed... should probably skip parsing them too
-  return array($vertexes, array(), $faces);
+  return $faces;
 }
 
 ?>
--- php/json.php
+++ php/json.php
@@ -1,14 +1,27 @@
 <?php
 
-// might need to be higher if server chokes on really really big models...
-set_time_limit(120);
+// server needs a good cpu!  Might need to make timeout higher if server chokes on really really big models...
+set_time_limit(300);
 
 include('convert.php');
 
 $file = $_GET['file'];
 
-// TODO: check for ascii/binary stl or obj file...
-$result = parse_stl_string($file);
+$file_parts = pathinfo($file);
+switch($file_parts['extension']){
+  case 'stl':
+    $contents = file_get_contents($file);    
+    $contents = preg_replace('/$\s+.*/', '', $contents);
+    if (stripos($contents, 'solid') === FALSE) {
+      $result = parse_stl_binary($file);
+    } else {
+      $result = parse_stl_string($file);
+    }  
+    break;
+  case 'obj':
+    $result = parse_obj_string($file);
+    break;
+}
 
 echo json_encode($result);
 
